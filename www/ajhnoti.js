16d8fa66e789f7aeda0fd08d284411d519d2d7d5
rns <cdark2030@gmail.com>
2023-02-26 22:08:03 +0300

    update

--- www/ajhnoti.js
+++ www/ajhnoti.js
@@ -4,10 +4,11 @@ exports.coolMethod = function (arg0, success, error) {
     exec(success, error, 'ajhnoti', 'coolMethod', [arg0]);
 };
 
-exports.checkNotificationPermission = function (arg0, error) {
-    exec((json)=>{
-        return json.checkNotificationPermission;
-    }, error, 'ajhnoti', 'checkNotificationPermission', [arg0]);
+exports.checkNotificationPermission = function (arg0, success, error) {
+    //(json)=>{
+       // return json.checkNotificationPermission;
+   // }
+    exec(success, error, 'ajhnoti', 'checkNotificationPermission', [arg0]);
 
 };
 
